cc484df699fdc8170f07998c6ee30567b126c304
Craig Kaminsky <imageaid@gmail.com>
2025-08-20 13:17:02 -0600

    Update Ruby version and streamline project setup
    Updated the Ruby version to 3.4.4 in the `.ruby-version` file. Removed unnecessary gems and commands, reorganized and simplified the setup process, and refined route and resource definitions. Also, adjusted configurations for Procfile creation and added tailored commands for user authentication, migrations, and other essential features.

--- base_template.rb
+++ base_template.rb
@@ -1,27 +1,16 @@
 gem "friendly_id"
 gem "mission_control-jobs"
 gem "pagy"
-gem "solid_cache"
-gem "solid_errors"
-gem "solid_queue"
 
 gem_group :development do
-  gem "annotate"
+  gem "annotaterb"
   gem "erb_lint"
-  gem "hotwire-livereload"
+  gem "hotwire-spark"
   gem "letter_opener_web", "~> 2.0"
   gem "rails_services"
 end
 
-file ".rubocop.yml", <<-CODE
-  # Omakase Ruby styling for Rails
-  inherit_gem:
-    rubocop-rails-omakase: rubocop.yml
-  # Your own specialized rules go here
-CODE
-
 append_to_file ".gitignore", <<-CODE
-
 # ignore Procfile.dev and IDE specific
 .byebug_history
 yarn-error.log
@@ -36,74 +25,32 @@ yarn-debug.log*
 Procfile.*
 CODE
 
-# setup the importmaps and pin some base JS packages
-rails_command "importmap:install"
-run 'bin/importmap pin "@hotwired/turbo-rails"'
-run 'bin/importmap pin "@hotwired/stimulus"'
-run 'bin/importmap pin "alpinejs"'
-
-# setup CSS
-rails_command "tailwindcss:install"
-
-# storage, text, errors, cache and queues
-rails_command "active_storage:install"
-rails_command "action_text:install"
-rails_command "generate solid_errors:install"
-rails_command "generate solid_queue:install"
-rails_command "solid_cache:install:migrations"
-
-append_to_file "config/importmap.rb", <<-CODE
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/services", under: "services"
-CODE
-
 environment "config.active_job.queue_adapter = :solid_queue", env: "production"
 environment "config.mission_control.jobs.adapters = [ :solid_queue ]", env: "production"
 environment "config.active_job.queue_adapter = :solid_queue", env: "development"
 environment "config.mission_control.jobs.adapters = [ :solid_queue ]", env: "development"
 
-# not going with foreman/Procfile for SolidQueue for now
-# append_to_file "Procfile.dev", <<-CODE
-# queue: bin/rails solid_queue:start
-# CODE
-
-# using the puma plugin for SolidQueue atm
-append_to_file "config/puma.rb", <<-CODE
-if Rails.env.production?
-  # Allow solid_queue to be restarted with the `bin/rails restart` command.
-  plugin :solid_queue
+if File.exist?("Procfile.dev")
+  append_to_file "Procfile.dev", <<-CODE
+    worker: bin/jobs
+  CODE
+else
+  create_file "Procfile.dev", ""
+  append_to_file "Procfile.dev", <<-CODE
+    web: bin/rails s -p 3000 -b 0.0.0.0
+    css: yarn build:css --watch
+    worker: bin/jobs
+  CODE
 end
-CODE
 
-generate(:scaffold, "user", "first_name:string:index", "last_name:string:index", "name:string", "email:string:uniq", "role:integer", "password_digest:string", "slug:string:uniq")
-inject_into_class("app/models/user.rb", "User", "  extend FriendlyId\n  friendly_id :name, use: :slugged\n\n  has_secure_password\n  generates_token_for :password_reset, expires_in: 15.minutes { password_salt&.last(10) }\n  generates_token_for :email_confirmation, expires_in: 24.hours { email }\n\n  normalizes :email, with: -> email { email.strip.downcase }\n\n  enum role: { guest: 0, admin: 1 }\n\n  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }\n")
+rails_command "css:install:tailwind"
+rails_command "generate authentication"
+rails_command "generate migration AddFieldsToUsers first_name:string:index last_name:string:index name:string role:integer slug:string:uniq"
+inject_into_class("app/models/user.rb", "User", "  extend FriendlyId\n  friendly_id :name, use: :slugged\n\n  generates_token_for :password_reset, expires_in: 15.minutes { password_salt&.last(10) }\n  generates_token_for :email_confirmation, expires_in: 24.hours { email }\n\n  normalizes :email, with: -> email { email.strip.downcase }\n\n  enum role: { guest: 0, admin: 1 }\n\n  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }\n")
 
-# copy the base session, registration and password related views and files
-directory "app", force: true
-copy_file "./public/icon-192x192.png"
-copy_file "./public/icon-512x512.png"
-
-# routes
-route 'root to: "welcome#index"'
-route "resource :session, only: [ :new, :create, :destroy ]"
-route "resource :login, only: [ :new, :show, :create, :destroy ]"
-route "resource :registration, only: [ :new, :create ]"
-route "resource :password, only: [ :edit, :update ]"
-route "resource :password_reset, only: [ :new, :create, :edit, :update ]"
-route 'get "/offline", to: "pwa#offline", as: :pwa_offline'
-route 'get "service-worker" => "pwa#service_worker", as: :pwa_service_worker'
-route 'get "manifest" => "pwa#manifest", as: :pwa_manifest'
-route 'post "/notifications/subscribe", to: "pwa#subscribe"'
-
-route <<-CODE
-  mount SolidErrors::Engine, at: "/solid_errors"
-  mount MissionControl::Jobs::Engine, at: "/jobs"
-CODE
-
-rails_command "generate annotate:install"
+rails_command "generate annotate_rb:install"
 rails_command "generate friendly_id"
-rails_command "db:create"
-rails_command "db:migrate"
+rails_command "db:prepare"
 
 inject_into_class("app/controllers/application_controller.rb", "ApplicationController", "  include Pagy::Backend\n")
 inject_into_module("app/helpers/application_helper.rb", "ApplicationHelper", "  include Pagy::Frontend\n")
@@ -122,13 +69,13 @@ initializer "solid_errors.rb", <<-CODE
 # Set authentication credentials for Solid Errors
 # Rails.application.config.solid_errors.username = Rails.application.credentials.solid_errors.username
 # Rails.application.config.solid_errors.password = Rails.application.credentials.solid_errors.password
-
-Rails.application.config.solid_errors.send_emails = true
-Rails.application.config.solid_errors.email_from = "errors@localhost"
-Rails.application.config.solid_errors.email_to = "devs@localhost"
 CODE
 
 append_to_file ".rubocop.yml", <<-CODE
+# Omakase Ruby styling for Rails
+inherit_gem:
+  rubocop-rails-omakase: rubocop.yml
+# Your own specialized rules go here
 AllCops:
   Exclude:
     - '**/db/migrate/*'
@@ -186,6 +133,40 @@ CODE
 after_bundle do
   # Make sure Linux is in the Gemfile.lock for deploying
   run "bundle lock --add-platform x86_64-linux"
+
+  # routes
+  route 'root to: "welcome#index"'
+  route "resource :session, only: [ :new, :create, :destroy ]"
+  route "resource :login, only: [ :new, :show, :create, :destroy ]"
+  route "resource :registration, only: [ :new, :create ]"
+  route "resource :password, only: [ :edit, :update ]"
+  route "resource :password_reset, only: [ :new, :create, :edit, :update ]"
+  route 'get "/offline", to: "pwa#offline", as: :pwa_offline'
+  route 'get "service-worker" => "pwa#service_worker", as: :pwa_service_worker'
+  route 'get "manifest" => "pwa#manifest", as: :pwa_manifest'
+  route 'post "/notifications/subscribe", to: "pwa#subscribe"'
+
+  route <<-CODE
+    mount SolidErrors::Engine, at: "/solid_errors"
+    mount MissionControl::Jobs::Engine, at: "/jobs"
+  CODE
+
+  # setup CSS
+  rails_command "tailwindcss:install"
+  rails_command "active_storage:install"
+  rails_command "action_text:install"
+  rails_command "generate solid_errors:install"
+  rails_command "generate solid_queue:install"
+  rails_command "solid_cache:install:migrations"
+
+  append_to_file "config/importmap.rb", <<-CODE
+  pin_all_from "app/javascript/controllers", under: "controllers"
+  pin_all_from "app/javascript/services", under: "services"
+  CODE
+
+  # copy the base icons, session, registration and password related views and files
+  directory "public", force: true
+  directory "app", force: true
 
   # init the git repo
   git :init
